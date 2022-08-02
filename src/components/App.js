32f070de4e07068871a38429416a6a3f97258d90
Fekete-Bruno <brunohffekete@gmail.com>
2022-08-02 09:55:41 -0300

    Started globalStyles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import GlobalStyle from "../theme/globalStyles";
+import LoginScreen from "./LoginScreen/LoginScreen";
+
 export default function App(){
     return(
-        <div>Hello World</div>
+        <Fragment>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginScreen />} />
+                </Routes>
+            </BrowserRouter>
+        </Fragment>
     );
 }
---
+++ src/components/LoginScreen/LoginScreen.js
@@ -0,0 +1,5 @@
+export default function LoginScreen(){
+    return(
+        <div>Hello World!</div>
+    );
+}
