6bc5b368f301256ed4984e5bf5ecc0af4ddba4e5
Fekete-Bruno <brunohffekete@gmail.com>
2022-08-03 16:11:36 -0300

    Sign Up page is working fine

--- src/components/SignupScreen/SignupScreen.js
+++ src/components/SignupScreen/SignupScreen.js
@@ -1,30 +1,89 @@
 import {FormWrapper, LogButton} from "../../styles/Form Wrapper";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
 import logo from "../../images/TrackIt-Logo.png";
+import { postSignup } from "../../services/axios";
+
 
 export default function SignupScreen(){
-    const [disabled,setDisabled] = useState(false)
-    const [innerButton,setInnerButton] = useState('Sign Up')
+    const [disabled,setDisabled] = useState(false); 
+    const [innerButton,setInnerButton] = useState('Sign Up');
+    const [form,setForm] = useState({});
+    const navigate = useNavigate();
 
-    function formHandler(event){
+    function handleForm({name,value}){
+        setForm({
+            ...form,
+            [name]: value,
+          });
+    }
+
+    function sendForm(event){
         event.preventDefault();
-        console.log('hey');
         setDisabled(true);
         setInnerButton(<ThreeDots color="white"/>);
+        const request = postSignup(form);
+        request.then(()=>navigate("/"));
+        request.catch((res)=>resetForm(res.response));
+    }
+
+    function resetForm(resp){
+        const initial=0;
+        const txt = 'Attention: ';
+        if(resp.data.details){
+            alert(txt+resp.data.details[initial]);
+        } else if(resp.data.message){
+            alert(txt+resp.data.message);
+        } else {
+            alert('ERROR '+resp.status);
+        }
+        setDisabled(false);
+        setInnerButton('Sign Up');
     }
 
     return(
         <FormWrapper>
             <img src={logo} alt="TrackIt Logo" />
 
-            <form onSubmit={formHandler}>
-		    <input type="email" placeholder="email" required disabled={disabled} />
-		    <input type="password" placeholder="password" required disabled={disabled} />
-		    <input type="text" placeholder="name" required disabled={disabled} />
-		    <input type="text" placeholder="image" required disabled={disabled} />
-		    <button type="submit">{innerButton}</button>
+            <form onSubmit={sendForm}>
+		    <input type="email" placeholder="email" required disabled={disabled} onChange={
+                (e)=>{
+                    handleForm({
+                        name:e.target.placeholder,
+                        value:e.target.value,
+                    }
+                    );
+                }
+            }/>
+		    <input type="password" placeholder="password" required disabled={disabled} onChange={
+                (e)=>{
+                    handleForm({
+                        name:e.target.placeholder,
+                        value:e.target.value,
+                    }
+                    );
+                }
+            }/>
+		    <input type="text" placeholder="name" required disabled={disabled} onChange={
+                (e)=>{
+                    handleForm({
+                        name:e.target.placeholder,
+                        value:e.target.value,
+                    }
+                    );
+                }
+            }/>
+		    <input type="text" placeholder="image" required disabled={disabled} onChange={
+                (e)=>{
+                    handleForm({
+                        name:e.target.placeholder,
+                        value:e.target.value,
+                    }
+                    );
+                }
+            }/>
+		    <button type="submit" disabled={disabled}>{innerButton}</button>
 		    </form>
             
             <Link to="/">
