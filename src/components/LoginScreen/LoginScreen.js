bf055e14979a169f21a7a7eed87c31b7a71eefd5
Fekete-Bruno <brunohffekete@gmail.com>
2022-08-04 07:14:13 -0300

    Login Screen disable button

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom";
 import {FormWrapper , LogButton} from "../../styles/Form Wrapper";
 import logo from "../../images/TrackIt-Logo.png";
+import { useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
 
 export default function LoginScreen(){
+    const [disabled, setDisabled] = useState(false);
+    const [innerButton,setInnerButton] = useState('Log In');
+
+    function sendForm(event){
+        event.preventDefault();
+        setDisabled(true);
+        setInnerButton(<ThreeDots color="white"/>);
+    }
+    
     return(
         <FormWrapper>
             <img src={logo} alt="TrackIt Logo" />
 
-            <form onSubmit={(event)=>{
-                event.preventDefault();
-                console.log('hey');
-                }}>
-		    <input type="email" placeholder="email" required />
-		    <input type="password" placeholder="password" required />
-		    <button type="submit">Log In</button>
+            <form onSubmit={sendForm}>
+		    <input type="email" placeholder="email" required disabled={disabled} />
+		    <input type="password" placeholder="password" required disabled={disabled} />
+		    <button type="submit" disabled={disabled}>{innerButton}</button>
 		    </form>
             
             <Link to="/signup">
