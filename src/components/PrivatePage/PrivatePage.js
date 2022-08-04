2cf5ab0b5970cd1dfd2d3e52bf0ff133d24bb373
Fekete-Bruno <brunohffekete@gmail.com>
2022-08-04 15:03:02 -0300

    Made expired login page \(OoO)/

--- src/components/PrivatePage/PrivatePage.js
+++ src/components/PrivatePage/PrivatePage.js
@@ -1,30 +1,33 @@
 import logo from "../../images/TrackIt-Logo.png";
+import styled from "styled-components";
+import { FormWrapper } from "../../styles/Form Wrapper";
+import { useNavigate } from "react-router-dom";
 
 const SEC = 1000;
 const HR = 3600*SEC;
 
-function renderError() {
-    localStorage.clear("trackit");
-    return (
-        <>
-            <img src={logo} alt="TrackIt Logo" />
-            <h1>SEU LOGIN EXPIROU</h1>
-        </>
-    );
-}
-
 export default function PrivatePage({ children }) {
-  
+    const navigate = useNavigate();
     const auth = JSON.parse(localStorage.getItem("trackit"));
     
+    function renderError() {
+        localStorage.clear("trackit");
+        return (
+            <Wrapper>
+                <img src={logo} alt="TrackIt Logo" />
+                <h1>Your login expired!</h1>
+                <button onClick={()=>{navigate("/");}}>Return to Login Screen</button>
+            </Wrapper>
+        );
+    }
+
     if (!auth) {
       return renderError();
     }
   
     const now = +new Date();
     const timeLogged = auth.timestamp;
-    console.log(now-timeLogged);
-    console.log(HR);
+    console.log(HR-(now-timeLogged));
 
     if (now - timeLogged <= HR) {
       return (
@@ -35,4 +38,17 @@ export default function PrivatePage({ children }) {
     } else {
       renderError();
     }
-  }
+}
+
+const Wrapper = styled(FormWrapper)`
+    margin: 0 auto;
+    width: 90vw;
+
+    &>*{
+        margin:2vh;
+    }
+
+    h1{
+        font-size: 3vh;
+    }
+`;
